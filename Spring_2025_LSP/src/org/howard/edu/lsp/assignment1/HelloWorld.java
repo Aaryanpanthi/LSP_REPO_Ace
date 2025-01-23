17d718bcc6f308165b3d96317eb433e394c45b00
Aaryan1604 <aryanpanthi3@gmail.com>
2025-01-23 07:17:39 -0500

    Added my Name

--- Spring_2025_LSP/src/org/howard/edu/lsp/assignment1/HelloWorld.java
+++ Spring_2025_LSP/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -1,9 +1,15 @@
 package org.howard.edu.lsp.assignment1;
 
+
+/**
+ * Aaryan Panthi
+ */
+
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("Hello World");
 
 	}
 
