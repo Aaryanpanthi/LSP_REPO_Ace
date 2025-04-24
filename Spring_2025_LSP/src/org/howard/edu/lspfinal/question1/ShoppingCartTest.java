e572deb65191f8d8a152829eeb366f01d0b3c9f5
Aaryan1604 <aryanpanthi3@gmail.com>
2025-04-24 18:30:40 -0400

    setting yp dependencies for quesiton 1 and cheking the commit

---
+++ Spring_2025_LSP/src/org/howard/edu/lspfinal/question1/ShoppingCartTest.java
@@ -0,0 +1,14 @@
+package org.howard.edu.lspfinal.question1;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ShoppingCartTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
